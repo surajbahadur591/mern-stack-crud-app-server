9450c0280542e9f53b5d366048f3607a222cb847
Suraj <surajbahadur58@gmail.com>
2023-02-11 21:43:49 +0530

    edit and delete user

---
+++ controller/getUserController.js
@@ -0,0 +1,58 @@
+import User from "../schema/userSchema.js";
+
+export const getUserController = async (request, response) => {
+  try {
+
+   const users = await User.find({})
+   response.status(200).json(users)
+  } catch (e) {
+    response.status(404).json({
+        message : e.message
+    })
+  }
+};
+
+
+export const editUserController = async (request, response) => {
+
+    const id = request.params.id;
+    console.log(id)
+    try {
+  
+     const users = await User.find({ _id : id})
+     response.status(200).json(users)
+    } catch (e) {
+      response.status(404).json({
+          message : e.message
+      })
+    }
+  };
+  export const editUserDataController = async (request, response) => {
+    let user = request.body
+    const id = request.params.id;
+    const editUser = new User(user)
+    try {
+  
+     await User.updateOne({ _id : id}, editUser)
+     response.status(201).json(editUser)
+    } catch (e) {
+      response.status(409).json({
+          message : e.message
+      })
+    }
+  };
+  
+  export const deleteUserDataController = async (request, response) => {
+    const id = request.params.id;
+    try {
+  
+     await User.deleteOne({ _id : id})
+     response.status(201).json({message : 'user deleted successfully'})
+    } catch (e) {
+      response.status(409).json({
+          message : e.message
+      })
+    }
+  };
+
+  
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,11 @@
 import express,{Router} from 'express'
 import { addUserController } from '../controller/addUserController.js';
-
+import { getUserController, editUserController , editUserDataController, deleteUserDataController} from '../controller/getUserController.js';
 const router = express.Router();
 
 router.post('/adduser', addUserController)
-
-export default router;
+router.get('/getuser', getUserController)
+router.get('/:id', editUserController)
+router.post('/:id', editUserDataController)
+router.delete('/:id', deleteUserDataController)
+export default router; 
